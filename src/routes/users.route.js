f71bc760c852a8175f2919359a02273bad9ba257
omardiehs <omar.dieh@wildcodeschool.com>
2022-12-18 21:38:03 +0100

    controllers and routes tested

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,7 +1,14 @@
-const { getAllUsers } = require("../controllers/users");
-
 const Router = require("express").Router();
 
+// require the controller
+const { getAllUsers } = require("../controllers/users");
+
+// hide this part :
+// Router.get("/", (req, res) => {
+//   res.send("Test users Routes");
+// });
+
+// use the controller with the route
 Router.get("/", getAllUsers);
 
 module.exports = Router;
